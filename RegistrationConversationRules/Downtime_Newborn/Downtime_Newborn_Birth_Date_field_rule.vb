33b548759c0c69a8bd2faa33c47839c834ed71c2
Jowell Sabino <jowell.sabino@gmail.com>
2018-11-02 13:11:45 -0400

    Downtime Newborn rules

---
+++ RegistrationConversationRules/Downtime_Newborn/Downtime_Newborn_Birth_Date_field_rule.vb
@@ -0,0 +1,29 @@
+dim default_prompt    as string
+dim daysdiff   as long
+dim marital    as double
+dim marital_cd as double
+
+'Get currentprompt value
+default_prompt  = cstr(getvalue(currentprompt))
+daysdiff = comparetocurrentdate(currentprompt)
+'Get marital status
+marital  = cdbl(getvalue("person.marital_type_cd"))
+
+if default_prompt = "0000000000000000" then
+  exit sub
+
+'Validates that the given birth date isn't in the future
+elseif futuredate(currentprompt) then
+  call message("The date entered is in the future.  Please re-enter the birth date.")
+  call setvalue(currentprompt, "0000000000000000")
+
+'Marital Status defaults to "single" if patient age is less than 14.
+elseif daysdiff > -5113 and marital = 0 then
+  marital_cd = displaykeycode("SINGLE", 38)
+  call setvalue("person.marital_type_cd", cdbl(marital_cd))
+
+'Date of birth cannot be greater than 125 years old
+elseif daysdiff < -45656 then
+  call message("Age cannot be greater than 125 years old.")
+  call setvalue(currentprompt, "0000000000000000")
+end if
---
+++ RegistrationConversationRules/Downtime_Newborn/Downtime_Newborn_start_rule.vb
@@ -0,0 +1,69 @@
+'*Rules to blank out the verify status fields of all insurances so the user is forced
+'to ask each time
+dim encntr_id2 as double
+encntr_id2 = getvalue("person.encounter.encntr_id")
+
+if encntr_id2 = 0 then
+call setvalue("person.subscriber_01.person.health_plan.verify_status_cd",0)
+call setvalue("person.subscriber_02.person.health_plan.verify_status_cd",0)
+call setvalue("person.subscriber_03.person.health_plan.verify_status_cd",0)
+end if
+
+'*Prevents user from inadvertantly selecting an already discharged encntr when trying to add a new one
+' and simply making updates to the discharged encntr
+
+dim DaysDiff as long
+DaysDiff = CompareToCurrentDate("person.encounter.disch_dt_tm")
+if DaysDiff < -0.5 then
+  call message("You have selected a discharged encounter.  You can not make changes to an encounter 12 hours post discharge. Please contact your supervisor.")
+  call exitconversation
+  exit sub
+end if
+
+'*Does not allow a new encntr to be added if there is already an active EMERGENCY, INPATIENT, OBSERVATION, OUTPATIENT or
+'* DAYSURGERY Encounter
+
+dim person_id    as double
+dim encntr_id    as double
+dim eclass_cd    as double
+dim etype_cd     as double
+dim class_cd     as double
+dim eid          as double
+dim str          as string
+dim msg          as string
+dim edclass_cd   as double 
+dim iclass_cd    as double
+dim oclass_cd    as double
+dim hclass_cd    as double
+dim dsclass_cd   as double
+
+'Gets person id and encntr_id
+person_id = getvalue("person.person_id")
+encntr_id = getvalue("person.encounter.encntr_id") 
+
+'Verifies if person exists and user is adding a new encounter 
+if person_id > 0 and encntr_id <= 0 then 
+     eid = 0 
+     edclass_cd = displaykeycode("EMERGENCY", 71) 
+     iclass_cd = displaykeycode("INPATIENT", 71)
+     oclass_cd = displaykeycode("OBSERVATION", 71) 
+     hclass_cd = displaykeycode("OUTPATIENT", 71)
+     dsclass_cd = displaykeycode("DAYSURGERY", 71)
+       str = "select e.encntr_id from encounter e where e.person_id = " 
+       str = str & Chr(13) & Chr(10) &   cstr(person_id) & " and (e.encntr_type_cd = "
+       str = str & Chr(13) & Chr(10) &   cstr(edclass_cd)  
+       str = str & Chr(13) & Chr(10) & " or e.encntr_type_cd = " & cstr(iclass_cd) 
+       str = str & Chr(13) & Chr(10) & " or e.encntr_type_cd = " & cstr(oclass_cd) 
+       str = str & Chr(13) & Chr(10) & " or e.encntr_type_cd = " & cstr(hclass_cd) 
+       str = str & Chr(13) & Chr(10) & " or e.encntr_type_cd = " & cstr(dsclass_cd) & ")"    
+       str = str & Chr(13) & Chr(10) & " and e.active_ind = 1" 
+       str = str & Chr(13) & Chr(10) & " and e.encntr_status_cd = 854"
+       str = str & Chr(13) & Chr(10) & " and e.beg_effective_dt_tm <= cnvtdatetime(curdate,curtime3)" 
+       str = str & Chr(13) & Chr(10) & " and e.end_effective_dt_tm >= cnvtdatetime(curdate,curtime3)" 
+       eid = cdbl(ccl(str, TRUE)) 
+       if eid > 0 then 
+     call message("Patient selected has an active Inpatient, Emergency, Observation, Day Surgery, or Hospice encounter. Please discharge those encounters before proceeding.") 
+     call exitconversation 
+    exit sub 
+  end if 
+end if
